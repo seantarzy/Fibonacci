46741d2ddc0e18157dc1eace66459cc6b9b82ffb
Sean Tarzy <seantarzy@gmail.com>
2020-07-29 12:01:52 -0400

    drew circle finally

--- index.js
+++ index.js
@@ -26,20 +26,25 @@ return fibArray
 
 }
 
-function outputFLowers(n){
+function outputFLowerPetals(n){
    let flowerContainer = document.getElementById('flower-container')
     for(let i =0; i<n; i++){
    let petal = document.createElement('img')
         petal.src = ('/Users/seantarzy/Development/Messing/fibonacci/purple-petal.png')
+        petal.id = `petal ${i}`
+        petal.className = 'petal'
    flowerContainer.appendChild(petal)
 }
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    drawCircle()
     let centerImage = document.getElementById('flower-center')
     console.log('DOM fully loaded and parsed');
-    console.log(centerImage)
-
+    let centerPosition = centerImage.getBoundingClientRect();
+    let x = centerPosition.left;
+    let y = centerPosition.top;
+    console.log(x,y, centerPosition)
 });
 
 
@@ -50,12 +55,29 @@ document.getElementById('fib-num-input').addEventListener('keypress', function (
         e.preventDefault()
         let fibArray = fib(e.target.value)
         let fibUltimate = fibArray[fibArray.length - 1]
-        outputFLowers(fibUltimate)
+        outputFLowerPetals(fibUltimate)
         let fibList = document.createElement('li')
         fibList.append(fibArray)
         listBox.append(fibList)
     }
+    console.log(document.getElementById('flower-center-canvas').getContext('2d'))
 });
 
+function drawCircle() {
+    console.log('no context')
+    var canvas = document.getElementById('flower-center-canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        var X = canvas.width / 2;
+        var Y = canvas.height / 2;
+        var R = 55;
+        ctx.beginPath();
+        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#FF0000';
+        ctx.stroke();
+        console.log('context')
+    }
 
+}
 
