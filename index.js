ca472d9aca6c769146c831031ef4f5a330b85a8a
Sean Tarzy <seantarzy@gmail.com>
2020-09-25 16:51:39 -0400

    clear petals and they render around

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ function outputFLowerPetals(n){
        petalDiv.style.position = "absolute";
         petalDiv.style.left = x + 'px'
         petalDiv.style.top = y + 'px'
-        console.log(x, y)
-        console.log(petalDiv.left, "petal position")
 }}
 else{
     return "that's too much, man!"
@@ -55,15 +53,21 @@ else{
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    drawCircle()
+    // drawCircle()
     let centerImage = document.getElementById('flower-center')
-    console.log('DOM fully loaded and parsed');
     let centerPosition = centerImage.getBoundingClientRect();
     let x = centerPosition.left;
     let y = centerPosition.top;
 });
 
-
+const clearPetals = ()=>{
+    let center = document.getElementById("flower-center")
+    // for(let child in center.children){
+        while(center.firstChild){
+        center.removeChild(center.firstChild)
+        }
+    // }
+}
 document.getElementById('fib-num-input').addEventListener('keypress', function (e) {
     let listBox = document.getElementById('list-box')
     listBox.innerText = "Fibonacci Sequence:"
@@ -71,29 +75,28 @@ document.getElementById('fib-num-input').addEventListener('keypress', function (
         e.preventDefault()
         let fibArray = fib(e.target.value)
         let fibUltimate = fibArray[fibArray.length - 1]
+        clearPetals()
         outputFLowerPetals(fibUltimate)
         let fibList = document.createElement('li')
         fibList.append(fibArray)
         listBox.append(fibList)
     }
-    console.log(document.getElementById('flower-center-canvas').getContext('2d'), "circle stuff")
 });
 
-function drawCircle() {
-    console.log('no context')
-    var canvas = document.getElementById('flower-center-canvas');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        var X = canvas.width / 2;
-        var Y = canvas.height / 2;
-        var R = 55;
-        ctx.beginPath();
-        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = '#FF0000';
-        ctx.stroke();
-    }
+// function drawCircle() {
+//     var canvas = document.getElementById('flower-center-canvas');
+//     if (canvas.getContext) {
+//         var ctx = canvas.getContext('2d');
+//         var X = canvas.width / 2;
+//         var Y = canvas.height / 2;
+//         var R = 55;
+//         ctx.beginPath();
+//         ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+//         ctx.lineWidth = 3;
+//         ctx.strokeStyle = '#FF0000';
+//         ctx.stroke();
+//     }
 
 
-}
+// }
 
